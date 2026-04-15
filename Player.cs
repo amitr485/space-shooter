acaf3e2fdc5c9aed69a445be55f9dad63ffa8362
amitr485 <amit.rozinkovich@gmail.com>
2026-04-15 23:26:53 +0300

    Update Player.cs to improve player movement and rendering

--- Player.cs
+++ Player.cs
@@ -1,30 +1,36 @@
-using UnityEngine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
-public class Player : MonoBehaviour {
-    public float speed = 10f;
-    public GameObject bulletPrefab;
-    public Transform shootingPoint;
+namespace SpaceShooter
+{
+    public class Player
+    {
+        public Vector2 Position;
+        public int Width = 40;
+        public int Height = 40;
+        private float speed = 5f;
 
-    void Update() {
-        HandleMovement();
-        if (Input.GetButtonDown("Fire1")) {
-            Shoot();
+        public Rectangle Bounds => new Rectangle((int)Position.X, (int)Position.Y, Width, Height);
+
+        public Player()
+        {
+            Position = new Vector2(380, 550);
         }
-    }
 
-    void HandleMovement() {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput) * speed * Time.deltaTime;
-        transform.Translate(movement);
-    }
+        public void Update(KeyboardState keyboardState)
+        {
+            if (keyboardState.IsKeyDown(Keys.Left) || keyboardState.IsKeyDown(Keys.A))
+                Position.X -= speed;
+            if (keyboardState.IsKeyDown(Keys.Right) || keyboardState.IsKeyDown(Keys.D))
+                Position.X += speed;
 
-    void Shoot() {
-        Instantiate(bulletPrefab, shootingPoint.position, shootingPoint.rotation);
-    }
+            Position.X = MathHelper.Clamp(Position.X, 0, 800 - Width);
+        }
 
-    void OnDrawGizmos() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, new Vector3(1f, 1f, 1f));
+        public void Draw(SpriteBatch spriteBatch, Texture2D texture)
+        {
+            spriteBatch.Draw(texture, Bounds, Color.Yellow);
+        }
     }
 }
